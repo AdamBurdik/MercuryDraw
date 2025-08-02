92ddd01efaef2a94ebeed8ebb25ee69815ca354d
AdamIx <adamburdik9@gmail.com>
2025-08-02 18:40:18 +0200

    init

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("java")
+}
+
+group = "me.adamix.mercury.draw"
+version = "0.0.1"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.jetbrains:annotations:26.0.2")
+}
