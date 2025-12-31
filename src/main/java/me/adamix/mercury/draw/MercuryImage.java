98ca768337837d3ee3aba72d4587fd44fb546f40
Adam Burdík <adamburdik9@gmail.com>
2025-12-31 22:21:39 +0100

    Change: MercuryImage value to BufferedImage

--- src/main/java/me/adamix/mercury/draw/MercuryImage.java
+++ src/main/java/me/adamix/mercury/draw/MercuryImage.java
@@ -3,13 +3,14 @@ package me.adamix.mercury.draw;
 import org.jetbrains.annotations.NotNull;
 
 import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
 import java.awt.image.RenderedImage;
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-public record MercuryImage(@NotNull RenderedImage image) {
+public record MercuryImage(@NotNull BufferedImage image) {
 	public InputStream inputStream() throws IOException {
 		ByteArrayOutputStream out = new ByteArrayOutputStream();
 		ImageIO.write(image, "png", out);
